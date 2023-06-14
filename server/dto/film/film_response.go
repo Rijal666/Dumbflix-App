e5b9c4c8c55d476c27a11ec106f1f885abccde2c
Rijal666 <rizkirizalmualim2@gmail.com>
2023-06-14 12:09:13 +0700

    handle films

--- server/dto/film/film_response.go
+++ server/dto/film/film_response.go
@@ -5,7 +5,7 @@ import "dumbflix/models"
 type FilmResponse struct {
 	ID            int    `json:"id"`
 	Title         string `json:"title"`
-	ThumbnailFilm string `json:"thumbnail"`
+	ThumbnailFilm string `json:"thumbnail" form:"thumbnail"`
 	Year          int    `json:"year"`
 	CategoryId    int    `json:"category_id"`
 	Category      models.CategoryResponse `json:"category"`
--- server/models/film.go
+++ server/models/film.go
@@ -3,7 +3,7 @@ package models
 type Film struct {
 	ID            int              `json:"id" gorm:"type: int" `
 	Title         string           `json:"title" gorm:"type: varchar(255)" `
-	ThumbnailFilm string           `json:"thumbnail" gorm:"type: varchar(255)" `
+	ThumbnailFilm string           `json:"thumbnail" form:"thumbnail" gorm:"type: varchar(255)" `
 	Year          int              `json:"year" gorm:"type: int" `
 	CategoryId    int              `json:"category_id"`
 	Category      CategoryResponse `json:"category" gorm:"foreignKey: CategoryId"`
