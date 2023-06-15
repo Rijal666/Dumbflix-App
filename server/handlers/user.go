de8f20b3a38fa22d55c1734e2a59bea9c2342a40
Rijal666 <rizkirizalmualim2@gmail.com>
2023-06-16 00:21:58 +0700

    handle user

--- server/handlers/user.go
+++ server/handlers/user.go
@@ -8,7 +8,12 @@ import (
 	"dumbflix/repositories"
 	"fmt"
 	"net/http"
+	"os"
 
+	"context"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
@@ -93,6 +98,22 @@ func (h *handler) UpdateUser(c *gin.Context){
 		c.JSON(http.StatusInternalServerError, resultdto.ErrorResult{Status: http.StatusInternalServerError, Message: err.Error()})
 		return
 	}
+
+	var ctx = context.Background()
+		var CLOUD_NAME = os.Getenv("CLOUD_NAME")
+		var API_KEY = os.Getenv("API_KEY")
+		var API_SECRET = os.Getenv("API_SECRET")
+			
+		// Add your Cloudinary credentials ...
+		cld, _ := cloudinary.NewFromParams(CLOUD_NAME, API_KEY, API_SECRET)
+	
+		// Upload file to Cloudinary ...
+		resp, err := cld.Upload.Upload(ctx, dataFile, uploader.UploadParams{Folder: "Dumbflix"})
+	
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
 		if request.Fullname != "" {
 			user.Fullname = request.Fullname
 		}
@@ -112,7 +133,7 @@ func (h *handler) UpdateUser(c *gin.Context){
 			user.Address = request.Address
 		}
 		if request.Thumbnail != "" {
-			user.Thumbnail = request.Thumbnail
+			user.Thumbnail = resp.SecureURL
 		}
 
 		data, err := h.UserRepository.UpdateUser(user)
--- server/pkg/middleware/upload_file.go
+++ server/pkg/middleware/upload_file.go
@@ -1,66 +1,44 @@
 package middleware
 
 import (
-	"context"
-	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
-	"os"
 
-	"github.com/cloudinary/cloudinary-go/v2"
-	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"github.com/gin-gonic/gin"
 )
 
 func UploadFile(next gin.HandlerFunc) gin.HandlerFunc {
-	return func (c *gin.Context)  {
+	return func(c *gin.Context) {
 		file, err := c.FormFile("thumbnail")
-		// if file != nil {
-		if err != nil{
-			c.JSON(http.StatusBadRequest, err.Error())
+		if err != nil {
+			c.JSON(http.StatusBadRequest, err)
 			return
 		}
+
 		src, err := file.Open()
 		if err != nil {
-			c.JSON(http.StatusBadRequest, err.Error())
+			c.JSON(http.StatusBadRequest, err)
 			return
 		}
 		defer src.Close()
 
-		var ctx = context.Background()
-		var CLOUD_NAME = os.Getenv("CLOUD_NAME")
-		var API_KEY = os.Getenv("API_KEY")
-		var API_SECRET = os.Getenv("API_SECRET")
-			
-		// Add your Cloudinary credentials ...
-		cld, _ := cloudinary.NewFromParams(CLOUD_NAME, API_KEY, API_SECRET)
-	
-		// Upload file to Cloudinary ...
-		resp, err := cld.Upload.Upload(ctx, src, uploader.UploadParams{Folder: "Dumbflix"})
-	
+		tempFile, err := ioutil.TempFile("uploads", "image-*.png")
 		if err != nil {
-			fmt.Println(err.Error())
+			c.JSON(http.StatusBadRequest, err)
+			return
 		}
-		c.Set("dataFile", resp.SecureURL)
+		defer tempFile.Close()
+
+		if _, err = io.Copy(tempFile, src); err != nil {
+			c.JSON(http.StatusBadRequest, err)
+			return
+		}
+
+		data := tempFile.Name()
+		filename := data[8:] // split uploads/
+
+		c.Set("dataFile", filename)
 		next(c)
 	}
 }
-		// tempFile, err := ioutil.TempFile("uploads", "image-*.png")
-		// if err != nil {
-		// 	c.JSON(http.StatusBadRequest, err.Error())
-		// 	return
-		// }
-		// defer tempFile.Close()
-
-		// if _, err := io.Copy(tempFile, src); err != nil {
-		// 	c.JSON(http.StatusBadRequest, err.Error())
-		// 	return
-		// }
-		// data := tempFile.Name()
-
-		// update := strings.Split(data, "\\")[1]
-		// fmt.Println(update)
-
-// 		c.Set("dataFile", "")
-// 		next(c)
-// 	}
-// }
