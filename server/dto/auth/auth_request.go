7a6a1dbb9cb8079982691da9e1ce79e6010adfe8
Rijal666 <rizkirizalmualim2@gmail.com>
2023-06-15 23:53:20 +0700

    handle user

--- server/dto/auth/auth_request.go
+++ server/dto/auth/auth_request.go
@@ -1,14 +1,14 @@
 package authdto
 
 type AuthRequset struct {
-	IsAdmin  bool   `json:"is_admin"`
-	Fullname string `json:"fullname" validate:"required"`
-	Email    string `json:"email" validate:"required"`
-	Password string `json:"password" validate:"required"`
-	Gender   string `json:"gender" validate:"required"`
-	Phone    string `json:"phone" validate:"required"`
-	Address  string `json:"address" validate:"required"`
-	Image    string `json:"image"`
+	IsAdmin   bool   `json:"is_admin"`
+	Fullname  string `json:"fullname" validate:"required"`
+	Email     string `json:"email" validate:"required"`
+	Password  string `json:"password" validate:"required"`
+	Gender    string `json:"gender" validate:"required"`
+	Phone     string `json:"phone" validate:"required"`
+	Address   string `json:"address" validate:"required"`
+	Thumbnail string `json:"thumbnail"`
 }
 
 type LoginRequest struct {
