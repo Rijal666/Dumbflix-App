367586d4e87a6bc6e6723dd974911d444ae74223
Rijal666 <rizkirizalmualim2@gmail.com>
2023-06-16 00:39:21 +0700

    handle user

--- server/handlers/user.go
+++ server/handlers/user.go
@@ -8,12 +8,7 @@ import (
 	"dumbflix/repositories"
 	"fmt"
 	"net/http"
-	"os"
 
-	"context"
-
-	"github.com/cloudinary/cloudinary-go/v2"
-	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
@@ -98,22 +93,7 @@ func (h *handler) UpdateUser(c *gin.Context){
 		c.JSON(http.StatusInternalServerError, resultdto.ErrorResult{Status: http.StatusInternalServerError, Message: err.Error()})
 		return
 	}
-
-	var ctx = context.Background()
-		var CLOUD_NAME = os.Getenv("CLOUD_NAME")
-		var API_KEY = os.Getenv("API_KEY")
-		var API_SECRET = os.Getenv("API_SECRET")
-			
-		// Add your Cloudinary credentials ...
-		cld, _ := cloudinary.NewFromParams(CLOUD_NAME, API_KEY, API_SECRET)
 	
-		// Upload file to Cloudinary ...
-		resp, err := cld.Upload.Upload(ctx, dataFile, uploader.UploadParams{Folder: "Dumbflix"})
-	
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-
 		if request.Fullname != "" {
 			user.Fullname = request.Fullname
 		}
@@ -133,7 +113,7 @@ func (h *handler) UpdateUser(c *gin.Context){
 			user.Address = request.Address
 		}
 		if request.Thumbnail != "" {
-			user.Thumbnail = resp.SecureURL
+			user.Thumbnail = request.Thumbnail
 		}
 
 		data, err := h.UserRepository.UpdateUser(user)
