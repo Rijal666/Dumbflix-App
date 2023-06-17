548bd6442f539d01231d181e8cd8923e32105477
Rijal666 <rizkirizalmualim2@gmail.com>
2023-06-17 22:25:08 +0700

    handle films

--- server/models/episode.go
+++ server/models/episode.go
@@ -1,11 +1,12 @@
 package models
 
 type Episode struct {
-	ID            int    `json:"id" gorm:"primary_key:auto_increment"`
-	Title         string `json:"title"  gorm:"type: varchar(255)"`
-	ThumbnailFilm string `json:"thumbnail" gorm:"type: varchar(255)"`
-	LinkFilm      string `json:"link" gorm:"type: varchar(255)"`
-	FilmId        int    `json:"film_id" `
+	ID            int          `json:"id" gorm:"primary_key:auto_increment"`
+	Title         string       `json:"title"  gorm:"type: varchar(255)"`
+	ThumbnailFilm string       `json:"thumbnail" gorm:"type: varchar(255)"`
+	LinkFilm      string       `json:"link" gorm:"type: varchar(255)"`
+	FilmId        int          `json:"film_id" `
+	Film          FilmResponse `json:"film"`
 }
 
 type EpisodeResponse struct {
